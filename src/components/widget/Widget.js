e4fa5dd8736a03f672929c30b16df488b3d91369
Christopher Berry <cberry191@hotmail.com>
2022-09-03 23:11:12 +1000

    planned charts

--- src/components/widget/Widget.js
+++ src/components/widget/Widget.js
@@ -21,7 +21,10 @@ const Widget = ({ type }) => {
                 title: "USERS",
                 isMoney: false,
                 link: "See All Users",
-                icon: <PeopleAltIcon className="icon"/>,
+                icon: <PeopleAltIcon className="icon" style={{
+                    backgroundColor: "rgba(255,0,0,0.2)", 
+                    color: "red"
+                }}/>,
             };
             break;
         case "order":
@@ -29,7 +32,10 @@ const Widget = ({ type }) => {
                 title: "ORDERS",
                 isMoney: true,
                 link: "See All Orders",
-                icon: <ShoppingBasketIcon className="icon"/>,
+                icon: <ShoppingBasketIcon className="icon" style={{
+                    backgroundColor: "rgba(218, 165, 32 ,0.2)", 
+                    color: "goldenrod"
+                }}/>,
             };
         break;
         case "earning":
@@ -37,7 +43,10 @@ const Widget = ({ type }) => {
                 title: "EARNINGS",
                 isMoney: true,
                 link: "See Earnings",
-                icon: <ReceiptIcon className="icon"/>,
+                icon: <ReceiptIcon className="icon" style={{
+                    backgroundColor: "rgba(128,0,128,0.2)", 
+                    color: "purple"
+                }}/>,
             };
         break;
         case "balance":
@@ -45,7 +54,10 @@ const Widget = ({ type }) => {
                 title: "BALANCE",
                 isMoney: true,
                 link: "See Balance",
-                icon: <SavingsIcon className="icon"/>,
+                icon: <SavingsIcon className="icon" style={{
+                    backgroundColor: "rgba(0,128,0,0.2)", 
+                    color: "green"
+                }}/>,
             };
         break;
         default:
